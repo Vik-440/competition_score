5c83ce70e74a26affcb934973da7caf0eb96067c
Vik <v.paradyuk@gmail.com>
2023-08-26 18:02:27 +0300

    implement connect DB

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""Fnc for first testing system and create TTD"""
+
+
+def add(x, y):
+    """Add x fnd y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract numbers for test TDD."""
+    return x - y
