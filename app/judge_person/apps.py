e03fe7396c37ea8327fa950a8a0c92da79222107
Viktor <v.paradyuk@gmail.com>
2023-09-16 20:22:31 +0300

    start app Judge

---
+++ app/judge_person/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JudgePersonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'judge_person'
--- app/judge_person/models.py
+++ app/judge_person/models.py
@@ -2,24 +2,24 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
-class SportsPerson(models.Model):
-    """SportsPerson object."""
+class JudgePerson(models.Model):
+    """JudgePerson object."""
 
     first_name = models.CharField(
         max_length=50, validators=[MinLengthValidator(3)])
     last_name = models.CharField(
         max_length=50, validators=[MinLengthValidator(3)])
     birth_day = models.DateField()
-    city = models.CharField(
-        max_length=50, validators=[MinLengthValidator(3)], default=None)
+    # city = models.CharField(
+    #     max_length=50, validators=[MinLengthValidator(3)], default=None)
     team = models.CharField(
         max_length=50, validators=[MinLengthValidator(3)], default=None)
     rank = models.CharField(
         max_length=50, validators=[MinLengthValidator(3)], default=None)
+    license_expiration_date = models.DateField()
 
     def __str__(self):
-        # return self.last_name
-        return f'{self.last_name} {self.first_name}, {self.team}, {self.team}'
+        return f'{self.last_name} {self.first_name}, {self.rank}'
 
     class Meta:
         unique_together = ('first_name', 'last_name', 'birth_day')
