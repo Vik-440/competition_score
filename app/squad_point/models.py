a72fff34166bf5d27ee0642bd40719e544b41a3c
Viktor <v.paradyuk@gmail.com>
2024-01-25 15:09:00 +0200

    fix point in DB

--- app/squad_point/models.py
+++ app/squad_point/models.py
@@ -3,9 +3,14 @@
 from django.db.models import (
     Model,
     ForeignKey,
-    CASCADE
+    DecimalField,
+    IntegerField,
+    CASCADE,
 )
-
+from django.core.validators import(
+    MinValueValidator,
+    MaxValueValidator,
+)
 from judge_person.models import JudgePerson
 from squad.models import Squad
 
@@ -14,6 +19,10 @@ class SquadPoint(Model):
     """Table for points by performance"""
     squad_id = ForeignKey(Squad, on_delete=CASCADE, null=False)
     judge_person_id = ForeignKey(JudgePerson, on_delete=CASCADE, null=False)
+    point = IntegerField(
+        null=False,
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+    )
 
     class Meta:
         unique_together = ('squad_id', 'judge_person_id')
